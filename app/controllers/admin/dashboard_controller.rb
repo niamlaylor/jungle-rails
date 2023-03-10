6b0eee3cac0b70dd122093ae9d960fe10e193634
Liam Naylor <liam@liamnaylor.com>
2023-03-09 16:06:15 -0800

    Add dashboard controller product count variable that appears on view

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["HTTP_ADMIN_USER"], password: ENV["HTTP_ADMIN_PASS"]
 
   def show
+    @product_count = Product.count
   end
+
 end
